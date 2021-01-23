06c08eae10f418701ca6e494f02df7506654eacf
zhan006 <wzdx1024@gmail.com>
2021-01-23 12:43:46 +1100

    fairly large number supporting

--- src/fast-exponential.js
+++ src/fast-exponential.js
@@ -1,19 +1,22 @@
+
 module.exports = fastExp = (x, n)=>{
     //check if x and n integer
-    let _x = parseFloat(x)
-    let _n = parseInt(n)
-    //is not a number?
-    if(_x == NaN || _n == NaN) throw new Error("x and n should be integers")
-    //is float?
-    if(_n != n) throw new Error("n should not be float") ;
-    x = _x
-    n = _n
-    if(n < 0 ) return fastExp(1/x, -n);
+    if(typeof x != "number") throw new Error("x should be a number")
+    if(typeof n != "number") throw new Error("n should be a number")
+    if(n % 1 != 0) throw new Error("n should not be a float")
+    if(n < 0 ) return fastExp(1/x, -n)    
     else{
+        n = n > Number.MAX_SAFE_INTEGER ? BigInt(n) : n 
         let res = 1
         let bin = n.toString(2)
         for(i of bin){
+            if(res * res === Infinity) {
+                res = BigInt(res)
+                if(x % 1 != 0) throw new Error("does not support float multiplied by a BigInt")
+                x = BigInt(x)
+            }
             res = res * res
+            
             if(parseInt(i) === 1) res = res * x
         }
     return res

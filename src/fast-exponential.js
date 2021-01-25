1ef1532b6f8ce1e874c993d17f89170897f8e264
zhan006 <wzdx1024@gmail.com>
2021-01-25 14:50:59 +1100

    modular exponentiation

--- src/fast-exponential.js
+++ src/fast-exponential.js
@@ -1,24 +1,42 @@
 
-module.exports = fastExp = (x, n)=>{
-    //check if x and n integer
-    if(typeof x != "number") throw new Error("x should be a number")
-    if(typeof n != "number") throw new Error("n should be a number")
+module.exports = fastExp = (x, n, mod = null)=>{
+    //check if n is an integer
     if(n % 1 != 0) throw new Error("n should not be a float")
-    if(n < 0 ) return fastExp(1/x, -n)    
+    if(n < 0 ) return fastExp(1/x, -n, mod)
     else{
-        n = n > Number.MAX_SAFE_INTEGER ? BigInt(n) : n 
-        let res = 1
+        //convert to BigInt
+        try{
+            x = x % 1 === 0? BigInt(x) : x
+            n = BigInt(n)
+            mod = mod === null? null : BigInt(mod)
+    
+        }
+        catch(error){throw error}
+
+        let res = BigInt(1)
         let bin = n.toString(2)
         for(i of bin){
-            if(res * res === Infinity) {
-                res = BigInt(res)
-                if(x % 1 != 0) throw new Error("does not support float multiplied by a BigInt")
-                x = BigInt(x)
-            }
             res = res * res
-            
-            if(parseInt(i) === 1) res = res * x
+            if(parseInt(i) === 1) {
+                //x is a float and res is not so large
+                if(typeof x === "number" && res<Number.MAX_SAFE_INTEGER){
+                    
+                    res = Number(res) * x
+                }
+                //x if a float and res is so large
+                else if(typeof x === "number" && res>=Number.MAX_SAFE_INTEGER){
+                    throw new Error("the result is so large and x is a float")
+                }
+                else{
+                    res = res * x
+                }
+            }
+            if(mod) {try{
+                res = res % mod
+            }catch(error) {throw error}
         }
+        }
+    res = res > Number.MAX_SAFE_INTEGER? res : Number(res)
     return res
     }
 }
